00e212a180d0fbde4ce5aa40e7818def362d5117
Jake <jgraving@gmail.com>
2025-08-10 12:43:21 +0200

    Import backends with numpy as default

---
+++ unwrap/link/__init__.py
@@ -0,0 +1,34 @@
+# unwrap/link/__init__.py
+
+# Default: NumPy backend functions
+from .numpy import (
+    inverse_softplus,
+    softplus,
+    reciprocal_softplus,
+    inverse_reciprocal_softplus,
+    modulo,
+    shifted_modulo,
+    rectifier,
+    atan2,
+    LINKS as NUMPY_LINKS,
+)
+
+# Backend-specific LINKS
+from .bambi import LINKS as BAMBI_LINKS
+from .pymc import LINKS as PYMC_LINKS
+
+__all__ = [
+    # Default NumPy link functions
+    "inverse_softplus",
+    "softplus",
+    "reciprocal_softplus",
+    "inverse_reciprocal_softplus",
+    "modulo",
+    "shifted_modulo",
+    "rectifier",
+    "atan2",
+    # Backend-specific LINKS dicts
+    "NUMPY_LINKS",
+    "BAMBI_LINKS",
+    "PYMC_LINKS",
+]
