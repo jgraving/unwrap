9e097c0dac49aecd2e9ebf44818b8cd8030526c8
Jake Graving <jgraving@gmail.com>
2025-10-08 11:13:17 +0200

    Rename to circular modulo

--- unwrap/link/pymc.py
+++ unwrap/link/pymc.py
@@ -27,10 +27,11 @@ def modulo(eta):
     return eta % (2 * PI)
 
 
-def rotated_modulo(eta):
+def circular_modulo(eta):
     return ((eta + PI) % (2 * PI)) - PI
 
-shifted_modulo = rotated_modulo
+shifted_modulo = circular_modulo
+rotated_modulo = circular_modulo
 
 def rectifier(eta):
     return pt.maximum(EPS, eta)
@@ -52,14 +53,18 @@ LINKS = {
     ),  # Short alias
     "modulo": LinksContainer(modulo, modulo),
     "mod": LinksContainer(modulo, modulo),  # Alias for modulo
-    "shifted_modulo": LinksContainer(rotated_modulo, rotated_modulo),
+    "shifted_modulo": LinksContainer(shifted_modulo, shifted_modulo),
     "shift_mod": LinksContainer(
-        rotated_modulo, rotated_modulo
-    ),  # Alias for rotated_modulo
+        shifted_modulo, shifted_modulo
+    ),  # Alias for circular_modulo
     "rotated_modulo": LinksContainer(rotated_modulo, rotated_modulo),
     "rot_mod": LinksContainer(
         rotated_modulo, rotated_modulo
-    ),  # Alias for rotated_modulo
+    ),  # Alias for circular_modulo
+    "circular_modulo": LinksContainer(circular_modulo, circular_modulo),
+    "circ_mod": LinksContainer(
+        circular_modulo, circular_modulo
+    ),  # Alias for circular_modulo
     "rectifier": LinksContainer(rectifier, rectifier),
     "relu": LinksContainer(rectifier, rectifier),  # Alias for rectifier
     "atan2": LinksContainer(atan2, atan2),
